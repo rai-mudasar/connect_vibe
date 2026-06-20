9fe9f53125c868e58f5d483280afae450d3fbda6
rai-mudasar <raimudassar709@gmail.com>
2026-06-21 00:04:04 +0500

    remaining

--- src/models/conversationModel.js
+++ src/models/conversationModel.js
@@ -1,19 +1,30 @@
 import mongoose from "mongoose";
 
 const conversationSchema = new mongoose.Schema({
-    participants: [
-      {
-        type: mongoose.Schema.Types.ObjectId, ref: "user" 
-      }
-    ],
+  participants: [
+    {
+      type: mongoose.Schema.Types.ObjectId, ref: "user"
+    }
+  ],
 
-    lastMessage: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "message",
-    },
+  lastMessage: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "message",
   },
+
+  deletedFor: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      default: []
+    }
+  ]
+},
   { timestamps: true },
 );
+
+conversationSchema.index({ participants: 1 });
+conversationSchema.index({ deletedFor: 1 });
 
 const conversationModel =
   mongoose.models.conversation ||
