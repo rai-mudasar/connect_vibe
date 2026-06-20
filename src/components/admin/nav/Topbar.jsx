a62146b6d3fa9f5a6bfa718f01a5f66e478b599b
rai-mudasar <raimudassar709@gmail.com>
2026-06-21 00:13:14 +0500

    added dropdown to admin account center

--- src/components/admin/nav/Topbar.jsx
+++ src/components/admin/nav/Topbar.jsx
@@ -1,15 +1,15 @@
 "use client"
 
-import { useState } from "react"
-import { LogOut } from "lucide-react"
-import { signOut } from "next-auth/react"
-import { Avatar, AvatarFallback } from "@/components/ui/avatar"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import Link from "next/link"
 import SafeImage from "@/components/SafeImage"
+import { useState } from "react"
+import { signOut } from "next-auth/react"
+import { ChevronDown, House, LogOut } from "lucide-react"
+import { Avatar, AvatarFallback } from "@/components/ui/avatar"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 
 export default function Topbar({ user }) {
-  const [dropdownOpen, setDropdownOpen] = useState(false)
+  const [open, setOpen] = useState(false)
 
   return (
     <header className="h-14 bg-card dark:bg-gray-800 border-b border-border flex items-center px-4 md:px-6 gap-4 shrink-0">
@@ -42,42 +42,72 @@ export default function Topbar({ user }) {
         </button>
 
         <div className="relative">
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <div className="flex items-center gap-2 rounded-lg hover:bg-label  p-1.5 transition cursor-pointer group">
-                <Avatar className="w-10 h-10 border md:border-0 border-border bg-label">
+          <DropdownMenu open={open} onOpenChange={setOpen}>
+            <DropdownMenuTrigger asChild >
+              <div className="w-17.5 md:w-20 p-[0.5px] border border-border md:p-px bg-bg rounded-[19px] flex flex-row cursor-pointer items-center justify-between"
+              >
+                <Avatar className="w-9 md:w-10 h-9 md:h-10 border border-border bg-bg">
                   <SafeImage
                     src={user?.profileImageUrl !== "" ? user?.profileImageUrl : null}
                     fill
                     alt="User Profile Image"
                     className="object-contain"
                   />
-                  <AvatarFallback className={'text-md font-bold'}>{user?.firstName?.[0]}</AvatarFallback>
+                  <AvatarFallback className={'text-[22px] text-primary font-bold'}>{user?.firstName?.[0]}</AvatarFallback>
                 </Avatar>
-                <span className="text-sm font-medium text-label group-hover:text-label2 hidden sm:block">
-                  {user?.firstName + " " + user.lastName}
-                </span>
-                <svg className="w-4 h-4 text-label group-hover:text-label2" fill="none" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24">
-                  <polyline points="6 9 12 15 18 9" />
-                </svg>
+                <div>
+                  <ChevronDown className={`w-5 md:w-7 text-white duration-300 mr-1 ${open === true ? "rotate-180" : "text-red-500"}`} />
+                </div>
               </div>
             </DropdownMenuTrigger>
 
-            <DropdownMenuContent className={"w-50 bg-card dark:bg-gray-800 border border-border dark:border-gray-700 rounded-xl shadow-lg z-50 py-1 mr-5"}>
-              <DropdownMenuItem>
-                <Link href={'/home'} className="px-2 py-2 border-b border-border dark:border-gray-700 hover:bg-label rounded-md cursor-pointer">
-                  <p className="text-xs font-semibold text-secondary truncate">{user?.firstName + " " + user?.lastName}</p>
-                  <p className="text-xs text-label2 dark:text-gray-400 truncate">{user?.email}</p>
+            {open && <div className="fixed inset-0 bg-black/40 z-20"></div>}
+
+            <DropdownMenuContent className={'z-50 bg-bg backdrop-blur-xl shadow-2xl border-border rounded-xl text-secondary mr-5 md:mr-17 flex flex-col'}>
+              <DropdownMenuItem className={'w-60 md:w-70'}>
+                <div className="bg-bg border-2 border-primary rounded-full">
+                  <Avatar className="w-15 h-15 border-2 border-bg bg-bg">
+                    <SafeImage
+                      src={user?.profileImageUrl !== "" ? user?.profileImageUrl : null}
+                      fill
+                      alt="User Profile Image"
+                      className="object-contain"
+                    />
+                    <AvatarFallback className={'text-[22px] text-primary font-bold'}>{user?.firstName?.[0] + user?.lastName?.[0]}</AvatarFallback>
+                  </Avatar>
+                </div>
+                <div>
+                  <p className="font-bold">{user?.firstName + " " + user.lastName}</p>
+                  <p className="text-[9px] text-label">{"@" + user?.username}</p>
+                  <p className="text-[9px] text-label">{user?.email}</p>
+                </div>
+              </DropdownMenuItem>
+
+              <DropdownMenuItem className={''}>
+                <p className="text-[10px] text-label tracking-widest uppercase">Account</p>
+                <div className="w-full h-px bg-card"></div>
+              </DropdownMenuItem>
+
+              <DropdownMenuItem onClick={() => setOpen(!open)}>
+                <Link href={'/home'} className={`flex w-full h-10 pl-2 rounded-lg cursor-pointer flex-row items-center gap-2 hover:bg-card/30 hover:text-primary hover:border border-border/30 font-semibold`}>
+                  <div className="rounded-md bg-card-active p-1.5">
+                    <House className="w-5 md:w-6 h-5 md:h-6" />
+                  </div>
+                  <p>Home</p>
                 </Link>
               </DropdownMenuItem>
 
-              <DropdownMenuItem>
-                <button
-                  onClick={() => signOut({ callbackUrl: "/login" })}
-                  className="w-full text-left px-4 py-2 text-sm flex items-center gap-2 text-secondary hover:bg-label rounded-md cursor-pointer"
-                >
-                  <LogOut className="w-5 md:w-6 h-5 md:h-6" />
-                  <p>Log out</p>
+              <DropdownMenuItem className={'mt-3'}>
+                <p className="text-[10px] text-label tracking-widest uppercase">Others</p>
+                <div className="w-full h-px bg-card"></div>
+              </DropdownMenuItem>
+
+              <DropdownMenuItem onClick={() => setOpen(!open)}>
+                <button onClick={() => signOut()} className="w-full h-10 pl-2 rounded-lg cursor-pointer flex flex-row items-center gap-2 hover:bg-card/30 hover:text-primary hover:border border-border/30 font-semibold">
+                  <div className="rounded-md bg-card-active p-1.5">
+                    <LogOut className="w-5 md:w-6 h-5 md:h-6" />
+                  </div>
+                  <p>Log Out</p>
                 </button>
               </DropdownMenuItem>
             </DropdownMenuContent>
